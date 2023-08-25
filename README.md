747a9d74bdfa1ea120d6ad8127c8866767bb0546
Blake Wyatt <894305+xNul@users.noreply.github.com>
2023-08-25 01:40:42 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# code-llama-for-vscode
+# Code Llama for VSCode
+
+An API which mocks [llama.cpp](https://github.com/ggerganov/llama.cpp) to enable support for Code Llama with the
+[Continue Visual Studio Code extension](https://continue.dev/).
+
+As of the time of writing this is the only way to use Code Llama with VSCode locally without having to sign up or get
+an API key for a service. The only exception to this is Continue with [Ollama](https://ollama.ai/), but Ollama doesn't
+support Windows or Linux. On the other hand, Code Llama is completely cross-platform and will run wherever
+Meta's own [codellama](https://github.com/facebookresearch/codellama) code will run.
+
+Now let's set you up with a few steps!
+
+### Setup
+
+Prerequisites:
+- [Download and run one of the Code Llama Instruct models](https://github.com/facebookresearch/codellama)
+- [Install the Continue VSCode extension](https://marketplace.visualstudio.com/items?itemName=Continue.continue)
+
+After you are able to use both independently, we will glue them together with Code Llama for VSCode.
+
+Steps:
+1. Move `llamacpp_mock_api.py` to your [`codellama`](https://github.com/facebookresearch/codellama) folder and install Flask to your environment with `pip install flask`.
+2. Run `llamacpp_mock_api.py` with your [Code Llama Instruct torchrun command](https://github.com/facebookresearch/codellama#fine-tuned-instruction-models). For example:
+```
+torchrun --nproc_per_node 1 llamacpp_mock_api.py \
+    --ckpt_dir CodeLlama-7b-Instruct/ \
+    --tokenizer_path CodeLlama-7b-Instruct/tokenizer.model \
+    --max_seq_len 512 --max_batch_size 4
+```
+3. Type `/config` in VSCode with Continue and make changes to `config.py` so it looks like [this](https://continue.dev/docs/customization#local-models-with-ggml).
+
+Restart VSCode or reload the Continue extension and you should now be able to use Code Llama for VSCode!
---
+++ llamacpp_mock_api.py
@@ -0,0 +1,95 @@
+from typing import Optional
+
+import fire
+from flask import Flask, redirect, url_for, jsonify, request
+import torch.distributed as dist
+
+from llama import Llama
+
+def main(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    temperature: float = 0.2,
+    top_p: float = 0.95,
+    max_seq_len: int = 512,
+    max_batch_size: int = 8,
+    max_gen_len: Optional[int] = None,
+):
+    # Create our Code Llama object.
+    generator = Llama.build(
+        ckpt_dir=ckpt_dir,
+        tokenizer_path=tokenizer_path,
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+    )
+
+    # With torchrun and distributed PyTorch, multiple copies of this code
+    # can be run at once. We only want one of them (node 0) to have the Flask API
+    # and we will use it to control the rest.
+    if dist.get_rank() == 0:
+        app = Flask(__name__)
+
+        @app.route("/v1/completions", methods=["POST"])
+        def completions():
+            # Would be used by Continue to generate a relevant title corresponding
+            # to the model's response, however, the current prompt passed by Continue
+            # is not good at obtaining a title from Code Llama so we hardcode a generic title.
+            response = "Code Llama"
+            return jsonify({"choices": [{"text": response}]})
+
+        @app.route("/v1/chat/completions", methods=["POST"])
+        def chat_completions():
+            content = request.json
+            messages = content["messages"]
+            
+            # Continue does not follow the user-assistant turn constraints Code Llama
+            # needs. It has duplicate subsequent responses for a role. For example, a/u/u/a
+            # will be sent by Continue when Code Llama only supports u/a/u/a so we squash
+            # duplicate subsequent roles into a single message.
+            if messages[0]["role"] == "assistant":
+                messages[0]["role"] = "system"
+            last_role = None
+            remove_elements = []
+            for i in range(len(messages)):
+                if messages[i]["role"] == last_role:
+                    messages[i-1]["content"] += "\n\n" + messages[i]["content"]
+                    remove_elements.append(i)
+                else:
+                    last_role = messages[i]["role"]
+            for element in remove_elements:
+                messages.pop(element)
+
+            # Broadcast what should be processed to other nodes (acting as a C&C node).
+            dist.broadcast_object_list([[messages], max_gen_len, temperature, top_p])
+
+            # Start Code Llama inferencing
+            results = generator.chat_completion(
+                [messages],
+                max_gen_len=max_gen_len,
+                temperature=temperature,
+                top_p=top_p,
+            )
+
+            # Send JSON with Code Llama's response back to the VSCode Continue
+            # extension. Note the extension expects six characters preappended to the
+            # reponse JSON so we preappend the random string "onesix" to fulfill that requirement.
+            response = results[0]["generation"]["content"].strip()
+            return "onesix" + jsonify({"choices": [{"delta": {"role": "assistant", "content": response}}]}).get_data(as_text=True)
+
+        # Run the Flask API server
+        app.run(port=8000)
+    
+    # Nodes which are not node 0 wait for tasks.
+    else:
+        while True:
+            config = [None] * 4
+            try:
+                dist.broadcast_object_list(config)
+                generator.chat_completion(
+                    config[0], max_gen_len=config[1], temperature=config[2], top_p=config[3]
+                )
+            except:
+                pass
+
+if __name__ == "__main__":
+    fire.Fire(main)
